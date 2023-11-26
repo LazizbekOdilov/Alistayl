d8994a3c1a5808dcdd5fd1565c579040f375a143
SardorbekAbdubannopob <sardirbekabdubannopob>
2023-11-26 21:01:05 +0500

    New Alistayl django loyiha

---
+++ userapp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views import View
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, "page-user-login.html")
+
+class RegisterView(View):
+    def get(self, request):
+        return render(request, "page-user-register.html")
